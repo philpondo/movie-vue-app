25cb72a7b96f114a05a6e5379be109823aa61890
phil <phil.pondo@gmail.com>
2020-07-08 21:55:03 -0400

    navbar update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
       <router-link class="navbar-brand" to="/">Movie App</router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
