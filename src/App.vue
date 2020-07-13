fd92f1ac83e8c22f3dc8b73207803e388cdcafb8
phil <phil.pondo@gmail.com>
2020-07-12 20:01:46 -0400

    adds html theme to movie app

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,215 @@
 <template>
   <div id="app">
-
-    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-      <router-link class="navbar-brand" to="/">Movie App</router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/movies">All Movies</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/movies/new">New Movie</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link v-if="!isLoggedIn()" class="nav-link" to="/signup">Signup</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link v-if="!isLoggedIn()" class="nav-link" to="/login">Login</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link v-if="isLoggedIn()" class="nav-link" to="/logout">Logout</router-link>
-          </li>
-        </ul> 
-      </div>
-    </nav>
+    <header class="position-absolute w-100">
+        <div class="container">
+            <div class="top-header d-none d-sm-flex justify-content-between align-items-center">
+                <nav class="d-flex aic">
+                    <a href="/login" class="login"><i class="fa fa-user" aria-hidden="true"></i>Login</a>
+                    <ul class="nav social d-none d-md-flex">
+                        <li><a href="https://www.facebook.com/fh5co" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    </ul>
+                </nav>
+            </div>
+            <nav class="navbar navbar-expand-md navbar-light">
+                <a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt="Multipurpose"></a>
+                <div class="group d-flex align-items-center">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation"><span
+                            class="navbar-toggler-icon"></span></button>
+                    <a class="login-icon d-sm-none" href="#"><i class="fa fa-user"></i></a>
+                    <a class="cart" href="#"><i class="fa fa-shopping-cart"></i></a>
+                </div>
+                <a class="search-icon d-none d-md-block" href="#"><i class="fa fa-search"></i></a>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="/movies">All Movies</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/movies/new">New Movie</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/signup">Signup</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/logout">Logout</a></li>
+                    </ul>
+                    <form class="bg-white search-form" method="get" id="searchform">
+                        <div class="input-group">
+                            <input class="field form-control" id="s" name="s" type="text" placeholder="Search">
+                            <span class="input-group-btn">
+                                <input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit"
+                                    value="Search">
+                            </span>
+                        </div>
+                    </form>
+                </div>
+            </nav>
+        </div>
+    </header>
 
     <!-- individual components injected -->
     <div class="container">
       <router-view/>
+      <footer>
+        <!-- Widgets Start -->
+        <div class="footer-widgets">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-xl-3">
+                        <div class="single-widget contact-widget" data-aos="fade-up" data-aos-delay="0">
+                            <h6 class="widget-tiltle">&nbsp;</h6>
+                            <p>By subscribing to our mailing list you will always be update with the latest news from
+                                us.
+                            </p>
+                            <div class="media">
+                                <i class="fa fa-map-marker"></i>
+                                <div class="media-body ml-3">
+                                    <h6>Address</h6>
+                                    Level 13, 2 Elizabeth St,<br>
+                                    Melbourne, Victoria 3000 Australia
+                                </div>
+                            </div>
+                            <div class="media">
+                                <i class="fa fa-envelope-o"></i>
+                                <div class="media-body ml-3">
+                                    <h6>Have any questions?</h6>
+                                    <a href="mailto:support@steelthemes.com">Support@Steelthemes.com</a>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <i class="fa fa-phone"></i>
+                                <div class="media-body ml-3">
+                                    <h6>Call us & Hire us</h6>
+                                    <a href="tel:+610791803458"> +61 (0) 7 9180 3458</a>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <i class="fa fa-fax"></i>
+                                <div class="media-body ml-3">
+                                    <h6>Fax</h6>
+                                    <a href="fax:911889047521432">(91) 188904752 1432</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xl-3">
+                        <div class="single-widget twitter-widget" data-aos="fade-up" data-aos-delay="200">
+                            <h6 class="widget-tiltle">Fresh Tweets</h6>
+                            <div class="media">
+                                <i class="fa fa-twitter"></i>
+                                <div class="media-body ml-3">
+                                    <h6><a href="#">@Themes,</a> Html Version Out Now</h6>
+                                    <span>10 Mins Ago</span>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <i class="fa fa-twitter"></i>
+                                <div class="media-body ml-3">
+                                    <h6><a href="#">@Envato,</a> the best selling item of the day!</h6>
+                                    <span>20 Mins Ago</span>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <i class="fa fa-twitter"></i>
+                                <div class="media-body ml-3">
+                                    <h6><a href="#">@Collis,</a> We Planned to Update the Enavto Author Payment Method
+                                        Soon!</h6>
+                                    <span>10 Mins Ago</span>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <i class="fa fa-twitter"></i>
+                                <div class="media-body ml-3">
+                                    <h6><a href="#">@SteelThemes,</a> Html Version Out Now</h6>
+                                    <span>15 Mins Ago</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xl-3">
+                        <div class="single-widget recent-post-widget" data-aos="fade-up" data-aos-delay="400">
+                            <h6 class="widget-tiltle">Latest Updates</h6>
+                            <div class="media">
+                                <a class="rcnt-img" href="#"><img src="assets/images/rcnt-post1.png"
+                                        alt="Recent Post"></a>
+                                <div class="media-body ml-3">
+                                    <h6><a href="#">An engaging</a></h6>
+                                    <p><i class="fa fa-user"></i>Mano <i class="fa fa-eye"></i> 202 Views</p>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <a class="rcnt-img" href="#"><img src="assets/images/rcnt-post2.png"
+                                        alt="Recent Post"></a>
+                                <div class="media-body ml-3">
+                                    <h6><a href="#">Statistics and analysis. The key to succes.</a></h6>
+                                    <p><i class="fa fa-user"></i>Rosias <i class="fa fa-eye"></i> 20 Views</p>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <a class="rcnt-img" href="#"><img src="assets/images/rcnt-post3.png"
+                                        alt="Recent Post"></a>
+                                <div class="media-body ml-3">
+                                    <h6><a href="#">Envato Meeting turns into a photoshooting.</a></h6>
+                                    <p><i class="fa fa-user"></i>Kien <i class="fa fa-eye"></i> 74 Views</p>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <a class="rcnt-img" href="#"><img src="assets/images/rcnt-post4.png"
+                                        alt="Recent Post"></a>
+                                <div class="media-body ml-3">
+                                    <h6><a href="#">An engaging embedded the video posts</a></h6>
+                                    <p><i class="fa fa-user"></i>Robert <i class="fa fa-eye"></i> 48 Views</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xl-3">
+                        <div class="single-widget tags-widget" data-aos="fade-up" data-aos-delay="800">
+                            <h6 class="widget-tiltle">Popular Tags</h6>
+                            <a href="#">Amazing</a>
+                            <a href="#">Design</a>
+                            <a href="#">Photoshop</a>
+                            <a href="#">Art</a>
+                            <a href="#">Wordpress</a>
+                            <a href="#">jQuery</a>
+                        </div>
+                        <div class="single-widget subscribe-widget" data-aos="fade-up" data-aos-delay="800">
+                            <h6 class="widget-tiltle">Subscribe us</h6>
+                            <p>Sign up for our mailing list to get latest updates and offers</p>
+                            <form class="" method="get">
+                                <div class="input-group">
+                                    <input class="field form-control" name="subscribe" type="email"
+                                        placeholder="Email Address">
+                                    <span class="input-group-btn">
+                                        <button type="submit" name="submit-mail"><i class="fa fa-check"></i></button>
+                                    </span>
+                                </div>
+                            </form>
+                            <p>We respect your privacy</p>
+                            <ul class="nav social-nav">
+                                <li><a href="https://www.facebook.com/fh5co" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Widgets End -->
+        <!-- Foot Note Start -->
+        <div class="foot-note">
+            <div class="container">
+                <div
+                    class="footer-content text-center text-lg-left d-lg-flex justify-content-between align-items-center">
+                    <p class="mb-0" data-aos="fade-right" data-aos-offset="0">&copy; 2019 All Rights Reserved. Design by <a href="https://freehtml5.co/multipurpose" target="_blank" class="fh5-link">FreeHTML5.co</a>.</p>
+                    <p class="mb-0" data-aos="fade-left" data-aos-offset="0"><a href="#">Terms Of Use</a><a
+                            href="#">Privacy & Security
+                            Statement</a></p>
+                </div>
+            </div>
+        </div>
+        <!-- Foot Note End -->
+    </footer>
     </div>
     
   </div>
